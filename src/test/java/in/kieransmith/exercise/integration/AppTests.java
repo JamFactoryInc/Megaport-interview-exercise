67444b1b4aecac4281154bc069ec58fd0d0fb4d0
Kieran <kieran.jas.smith@gmail.com>
2022-01-20 15:02:15 +1000

    Made AppTests clean up after itself

--- src/test/java/in/kieransmith/exercise/integration/AppTests.java
+++ src/test/java/in/kieransmith/exercise/integration/AppTests.java
@@ -44,21 +44,24 @@ public class AppTests {
     @Test
     public void ReadFileWriteSortedFile_ExpectSortedFile() throws IOException, MalformedInputException {
         // Arrange
-        Path inputPath = FileIO.getUniquePath();
+        Path filePath = FileIO.getUniquePath();
         ArrayList<String> unsortedNames = new ArrayList<String>(
                 Arrays.asList("BAKER, THEODORE", "SMITH, ANDREW", "KENT, MADISON", "SMITH, FREDRICK"));
         ArrayList<String> desiredNames = new ArrayList<String>(
                 Arrays.asList("BAKER, THEODORE", "KENT, MADISON", "SMITH, ANDREW", "SMITH, FREDRICK"));
 
-        FileIO.write(inputPath, unsortedNames);
+        FileIO.write(filePath, unsortedNames);
 
         // Act
-        NameList names = NameList.from(FileIO.read(inputPath)).sort();
-        FileIO.write(inputPath, names.toStringArray());
+        NameList names = NameList.from(FileIO.read(filePath)).sort();
+        FileIO.write(filePath, names.toStringArray());
 
-        ArrayList<String> actualNames = FileIO.read(inputPath);
+        ArrayList<String> actualNames = FileIO.read(filePath);
 
         // Assert
         assertTrue(actualNames.equals(desiredNames));
+
+        // Clean
+        filePath.toFile().delete();
     }
 }
